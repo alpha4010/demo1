44bfb29246ab3a5193e4ddb6f39e9ab6e14a63fb
sai keerthan <saikeerthandubbaka7@gmail.com>
2022-11-05 22:33:52 +0530

    comment comit

--- demo.py
+++ demo.py
@@ -1,3 +1,7 @@
 print ("hello world ")
 a = input(" what is you NAME")
 print ("hello " + a +"   ,how are you sir") 
+ # hello bro this is to say hello, hopefully
+ #hello there
+ #bro 
+ #hello bro this is hard
